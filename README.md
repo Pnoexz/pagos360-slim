e82538b8d711fc5d3e86f8097f25dbce0ea962bb
Matias Pino <pnoexz@gmail.com>
2018-02-03 12:34:56 -0300

    Initial folder and file structure

---
+++ README.md
@@ -0,0 +1,5 @@
+###Git hook (recommended)
+
+
+```bash
+$ ln -s $(pwd)/pre-commit $(pwd)/.git/hooks/
---
+++ app/routes.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author Matias Pino <pnoexz@gmail.com>
+ * @license GPL v3.0
+ */
+
+$app->group('/api/v1', function () use ($app) {
+    $app->group('/status', function () use ($app) {
+        $app->get(
+            '',
+            new \GameDatabase\Controllers\Status()
+        );
+    });
+});
